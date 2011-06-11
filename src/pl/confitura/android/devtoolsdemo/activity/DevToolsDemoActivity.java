19e565979147ee7f65342fb412e61ee7bb9ba816
Mateusz Grzechociński <mateusz.grzechocinski@javart.eu>
2011-06-11 09:32:04 +0200

    roboguice 2

--- src/pl/confitura/android/devtoolsdemo/activity/DevToolsDemoActivity.java
+++ src/pl/confitura/android/devtoolsdemo/activity/DevToolsDemoActivity.java
@@ -1,7 +1,9 @@
 package pl.confitura.android.devtoolsdemo.activity;
 
 import pl.confitura.android.devtoolsdemo.R;
-import android.app.Activity;
+import roboguice.activity.RoboActivity;
+import roboguice.inject.InjectResource;
+import roboguice.inject.InjectView;
 import android.app.AlertDialog;
 import android.os.Bundle;
 import android.view.View;
@@ -9,22 +11,29 @@ import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.TextView;
 
-public class DevToolsDemoActivity extends Activity {
+public class DevToolsDemoActivity extends RoboActivity {
+
+	@InjectView(R.id.editText1)
+	private TextView editText;
+
+	@InjectView(R.id.button1)
+	private Button buttonOk;
+
+	@InjectResource(R.string.hello)
+	private String helloResource;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
 
-		final TextView editText = (TextView) findViewById(R.id.editText1);
-
-		Button buttonOk = (Button) findViewById(R.id.button1);
 		buttonOk.setOnClickListener(new OnClickListener() {
+
+			String msg = helloResource + "," + editText.getText() + "!";
 
 			public void onClick(View v) {
 				new AlertDialog.Builder(DevToolsDemoActivity.this)
-						.setMessage(editText.getText()).setCancelable(true)
-						.show();
+						.setMessage(msg).setCancelable(true).show();
 			}
 		});
 	}
